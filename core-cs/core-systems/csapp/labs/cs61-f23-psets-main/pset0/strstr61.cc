dfa21b7b4866519c48254d06face961984b585a9
Andro Ranogajec <ranogaet@gmail.com>
2025-12-17 23:15:04 +0200

    Csapp:Lab Season, warmup part 5(hard)

--- core-cs/core-systems/csapp/labs/cs61-f23-psets-main/pset0/strstr61.cc
+++ core-cs/core-systems/csapp/labs/cs61-f23-psets-main/pset0/strstr61.cc
@@ -3,7 +3,19 @@
 #include <cstdio>
 
 char* mystrstr(const char* s1, const char* s2) {
-    // Your code here
+    if (*s2 == '\0') return (char*) s1;
+    while(*s1 != '\0')
+    {
+        const char* p1 = s1;
+        const char* p2 = s2;
+        while(*p2 != '\0' &&  *p1 == *p2)
+        {
+            p1++;
+            p2++;
+        }
+        if (*p2 == '\0') return (char*) s1;
+        s1++;
+    }
     return nullptr;
 }
 
@@ -11,7 +23,7 @@ int main(int argc, char* argv[]) {
     assert(argc == 3);
     printf("strstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], strstr(argv[1], argv[2]));
-    /* printf("mystrstr(\"%s\", \"%s\") = %p\n",
+    printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));
-    assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2])); */
+    assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));
 }
