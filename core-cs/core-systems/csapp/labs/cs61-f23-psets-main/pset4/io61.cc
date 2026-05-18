884b73222e2bacdd9a723c340a6effec69f4358b
Andro Ranogajec <ranogaet@gmail.com>
2026-05-18 13:02:26 +0300

    CsApp lab4 Stdio phase 1
    Move fucking docs one line lower so I can fucking read
    function description in header files fuck

--- core-cs/core-systems/csapp/labs/cs61-f23-psets-main/pset4/io61.cc
+++ core-cs/core-systems/csapp/labs/cs61-f23-psets-main/pset4/io61.cc
@@ -21,7 +21,6 @@ struct io61_file {
 //    Returns a new io61_file for file descriptor `fd`. `mode` is either
 //    O_RDONLY for a read-only file or O_WRONLY for a write-only file.
 //    You need not support read/write files.
-
 io61_file* io61_fdopen(int fd, int mode) {
     assert(fd >= 0);
     io61_file* f = new io61_file;
@@ -33,7 +32,6 @@ io61_file* io61_fdopen(int fd, int mode) {
 
 // io61_close(f)
 //    Closes the io61_file `f` and releases all its resources.
-
 int io61_close(io61_file* f) {
     io61_flush(f);
     int r = close(f->fd);
@@ -45,7 +43,6 @@ int io61_close(io61_file* f) {
 // io61_readc(f)
 //    Reads a single (unsigned) byte from `f` and returns it. Returns EOF,
 //    which equals -1, on end of file or error.
-
 int io61_readc(io61_file* f) {
     unsigned char ch;
     ssize_t nr = read(f->fd, &ch, 1);
@@ -70,7 +67,6 @@ int io61_readc(io61_file* f) {
 //    Note that the return value might be positive, but less than `sz`,
 //    if end-of-file or error is encountered before all `sz` bytes are read.
 //    This is called a “short read.”
-
 ssize_t io61_read(io61_file* f, unsigned char* buf, size_t sz) {
     size_t nread = 0;
     while (nread != sz) {
@@ -92,7 +88,6 @@ ssize_t io61_read(io61_file* f, unsigned char* buf, size_t sz) {
 // io61_writec(f)
 //    Write a single character `c` to `f` (converted to unsigned char).
 //    Returns 0 on success and -1 on error.
-
 int io61_writec(io61_file* f, int c) {
     unsigned char ch = c;
     ssize_t nw = write(f->fd, &ch, 1);
@@ -110,7 +105,6 @@ int io61_writec(io61_file* f, int c) {
 //    a drive running out of space. In this case io61_write returns the
 //    number of characters written, or -1 if no characters were written
 //    before the error occurred.
-
 ssize_t io61_write(io61_file* f, const unsigned char* buf, size_t sz) {
     size_t nwritten = 0;
     while (nwritten != sz) {
@@ -134,7 +128,6 @@ ssize_t io61_write(io61_file* f, const unsigned char* buf, size_t sz) {
 //
 //    If `f` was opened read-only, `io61_flush(f)` returns 0. It may also
 //    drop any data cached for reading.
-
 int io61_flush(io61_file* f) {
     (void) f;
     return 0;
@@ -144,7 +137,6 @@ int io61_flush(io61_file* f) {
 // io61_seek(f, off)
 //    Changes the file pointer for file `f` to `off` bytes into the file.
 //    Returns 0 on success and -1 on failure.
-
 int io61_seek(io61_file* f, off_t off) {
     off_t r = lseek(f->fd, (off_t) off, SEEK_SET);
     // Ignore the returned offset unless it’s an error.
@@ -163,7 +155,6 @@ int io61_seek(io61_file* f, off_t off) {
 //    If `!filename`, returns either the standard input or the
 //    standard output, depending on `mode`. Exits with an error message if
 //    `filename != nullptr` and the named file cannot be opened.
-
 io61_file* io61_open_check(const char* filename, int mode) {
     int fd;
     if (filename) {
@@ -183,7 +174,6 @@ io61_file* io61_open_check(const char* filename, int mode) {
 
 // io61_fileno(f)
 //    Returns the file descriptor associated with `f`.
-
 int io61_fileno(io61_file* f) {
     return f->fd;
 }
@@ -192,7 +182,6 @@ int io61_fileno(io61_file* f) {
 // io61_filesize(f)
 //    Returns the size of `f` in bytes. Returns -1 if `f` does not have a
 //    well-defined size (for instance, if it is a pipe).
-
 off_t io61_filesize(io61_file* f) {
     struct stat s;
     int r = fstat(f->fd, &s);
