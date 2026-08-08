4cd298aa2d8eed2048d7d59efe1a7aeee0ede950
Andro Ranogajec <ranogaet@gmail.com>
2026-08-08 11:46:35 +0300

    Computer Networking chapter 5 ex. and some thoughts on algos

---
+++ random/algo_practice/1.py
@@ -0,0 +1,40 @@
+#Foundations (Arrays, Hash Maps, Strings)
+
+#first duplicate value 
+""" 
+HWNDE
+
+what I have 
+a string
+
+what I want
+
+a string
+first occurence of same letter
+
+what I need 
+set()
+
+what is the edge case
+nothing zero
+no match 
+single item 
+
+
+what I do
+assign string to a variable 
+assign set to a variable 
+if single item 
+ return ""
+if nothing or zero
+ return ""
+create a loop iterate thru the string 
+if item not in set 
+add item to set 
+else 
+return the item 
+"""
+def first_dup(s):
+    pass
+
+first_dup("a")
